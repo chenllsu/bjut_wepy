30cc1c250c5768c9c30b150236f8a88ee7644fe5
chenlong <502143496@qq,com>
2018-01-10 00:22:48 +0800

    scroll fuck

---
+++ dist/components/index/swiper.js
@@ -0,0 +1,45 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.default = undefined;
+
+var _wepy = require('./../../npm/wepy/lib/wepy.js');
+
+var _wepy2 = _interopRequireDefault(_wepy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var swiper = function (_wepy$component) {
+	_inherits(swiper, _wepy$component);
+
+	function swiper() {
+		var _ref;
+
+		var _temp, _this, _ret;
+
+		_classCallCheck(this, swiper);
+
+		for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+			args[_key] = arguments[_key];
+		}
+
+		return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = swiper.__proto__ || Object.getPrototypeOf(swiper)).call.apply(_ref, [this].concat(args))), _this), _this.data = {}, _this.methods = {
+			reach_top: function reach_top() {
+				console.log('test');
+			}
+		}, _temp), _possibleConstructorReturn(_this, _ret);
+	}
+
+	return swiper;
+}(_wepy2.default.component);
+
+exports.default = swiper;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN3aXBlci5qcyJdLCJuYW1lcyI6WyJzd2lwZXIiLCJkYXRhIiwibWV0aG9kcyIsInJlYWNoX3RvcCIsImNvbnNvbGUiLCJsb2ciLCJjb21wb25lbnQiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFDQzs7Ozs7Ozs7Ozs7O0lBQ3FCQSxNOzs7Ozs7Ozs7Ozs7OztvTEFDcEJDLEksR0FBSyxFLFFBQ0xDLE8sR0FBVTtBQUNUQyxjQUFXLHFCQUFNO0FBQ2hCQyxZQUFRQyxHQUFSLENBQVksTUFBWjtBQUNBO0FBSFEsRzs7OztFQUZ5QixlQUFLQyxTOztrQkFBcEJOLE0iLCJmaWxlIjoic3dpcGVyLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXHJcblx0aW1wb3J0IHdlcHkgZnJvbSAnd2VweSdcclxuXHRleHBvcnQgZGVmYXVsdCBjbGFzcyBzd2lwZXIgZXh0ZW5kcyB3ZXB5LmNvbXBvbmVudCB7XHJcblx0XHRkYXRhPXt9XHJcblx0XHRtZXRob2RzID0ge1xyXG5cdFx0XHRyZWFjaF90b3A6ICgpID0+IHtcclxuXHRcdFx0XHRjb25zb2xlLmxvZygndGVzdCcpO1xyXG5cdFx0XHR9XHJcblx0XHR9XHJcblx0fVxyXG4iXX0=
